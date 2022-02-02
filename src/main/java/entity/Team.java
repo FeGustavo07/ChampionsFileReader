bcdd9144f8bc7f949e6b5aa824cb7583acff46c9
DouglasHLima <douglashdelima@gmail.com>
2022-02-02 20:35:38 -0300

    feat: Team entity

---
+++ src/main/java/entity/Team.java
@@ -0,0 +1,13 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Team {
+    private String name;
+    private int score;
+}
