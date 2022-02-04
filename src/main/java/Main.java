95e01aabbfa97a77c56fe5628867cf8c659ea055
DouglasHLima <douglashdelima@gmail.com>
2022-02-04 01:20:02 -0300

    feat: read file and save list on repository

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,22 @@
+import entity.SoccerMatch;
+import repository.SoccerMatchRepository;
+import service.FileReaderService;
 
+import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
+
+
     public static void main(String[] args) {
+        FileReaderService fileReaderService = new FileReaderService();
+        SoccerMatchRepository repository = new SoccerMatchRepository();
+
+        List<SoccerMatch> matches = fileReaderService.read("src/main/resources/brasileirao2020.csv");
+        repository.addAll(matches);
+
+        System.out.println(repository.getAllRegisters());
+        System.out.println(LocalDate.parse("1995-10-12"));
 
     }
 }
--- src/main/java/service/FileReaderService.java
+++ src/main/java/service/FileReaderService.java
@@ -1,14 +1,33 @@
 package service;
 
 import com.fileManager.FileReader;
+import entity.SoccerMatch;
+import lombok.SneakyThrows;
 import lombok.val;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileReaderService {
 
     private final FileReader fileReader = new FileReader();
 
-    public void read(String uri){
+    public List<SoccerMatch> read(String uri){
          val fileResult = fileReader.handleFile(uri);
-
+        List<SoccerMatch> listResult = new ArrayList<>();
+        for (String[] line : fileResult) {
+            SoccerMatch match = SoccerMatch.builder()
+                    .client(line[0])
+                    .opponent(line[1])
+                    .clientScore(Integer.parseInt(line[2]))
+                    .opponentScore(Integer.parseInt(line[3]))
+                    .date(LocalDate.parse(line[4], DateTimeFormatter.ofPattern("dd/MM/yyyy")))
+                    .build();
+            listResult.add(match);
+        }
+        return listResult;
     }
 }
