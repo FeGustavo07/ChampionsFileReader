8da0336d86f72da531d1c851f8910cf19036541b
DouglasHLima <douglashdelima@gmail.com>
2022-02-05 23:26:17 -0300

    feat: DataService

--- src/main/java/controller/FileManagerController.java
+++ src/main/java/controller/FileManagerController.java
@@ -1,20 +1,37 @@
 package controller;
 
-import service.DataProcessing;
+import service.DataProcessorService;
+
 import java.io.File;
-import java.util.*;
+import java.util.Scanner;
 
 public class FileManagerController {
 
-    DataProcessing data = new DataProcessing();
+    private final String URI_SOURCE_FILE = "src/main/resources/matchesResult.csv";
+    private final String URI_PATH_SAVE_TEAMS_FILE = "src/main/resources/resultsByTeams/";
+    private final String URI_PATH_SAVE_CLASSIFICATION_FILE = "src/main/resources/championshipStandings/";
+
+    private final DataProcessorService data = new DataProcessorService();
 
     public void run() {
-        data.read(data.getURI_SOURCE_FILE());
-        data.fillsTeamTable();
-        data.WriteEachTeamsFile();
-        data.WriteClassificationFile();
+        readFile(URI_SOURCE_FILE);
+        writeEachTeamsFile(URI_PATH_SAVE_TEAMS_FILE);
+        writeClassificationFile(URI_PATH_SAVE_CLASSIFICATION_FILE);
         this.deleteFilesOption();
     }
+
+    public void readFile(String path) {
+        this.data.read(path);
+    }
+
+    public void writeEachTeamsFile(String uri) {
+        data.WriteEachTeamsFile(uri);
+    }
+
+    public void writeClassificationFile(String uri) {
+        data.WriteClassificationFile(uri);
+    }
+
 
     private void deleteFilesOption() {
         Scanner input = new Scanner(System.in);
@@ -23,8 +40,8 @@ public class FileManagerController {
         int option = input.nextInt();
 
         if (option == 1) {
-            File[] classification = new File(data.getURI_PATH_SAVE_CLASSIFICATION_FILE()).listFiles();
-            File[] teams = new File(data.getURI_PATH_SAVE_TEAMS_FILE()).listFiles();
+            File[] classification = new File(URI_PATH_SAVE_CLASSIFICATION_FILE).listFiles();
+            File[] teams = new File(URI_PATH_SAVE_TEAMS_FILE).listFiles();
             assert classification != null;
             for (File file : classification) {
                 file.delete();
--- src/main/java/service/DataProcessorService.java
+++ src/main/java/service/DataProcessorService.java
@@ -11,16 +11,12 @@ import java.util.*;
 
 import static java.util.stream.Collectors.groupingBy;
 
-@Getter
-public class DataProcessing {
 
-
-    private final String URI_SOURCE_FILE = "src/main/resources/santander811matchesResult.csv";
-    private final String URI_PATH_SAVE_TEAMS_FILE = "src/main/resources/resultsByTeams/";
-    private final String URI_PATH_SAVE_CLASSIFICATION_FILE = "src/main/resources/championshipStandings/";
+public class DataProcessorService {
 
     private final FileReaderService fileReaderService = new FileReaderService();
     private final FileWriterService fileWriterService = new FileWriterService();
+
     private final SoccerMatchRepository matchRepository = new SoccerMatchRepository();
     private final TeamBoardRepository teamBoardRepository = new TeamBoardRepository();
 
@@ -28,9 +24,22 @@ public class DataProcessing {
     public void read(String path) {
         HashSet<SoccerMatch> matches = fileReaderService.read(path);
         this.matchRepository.addAll(matches);
+        this.fillsTeamTable();
     }
 
-    public void fillsTeamTable() {
+    public void WriteEachTeamsFile(String uri) {
+        teamBoardRepository.getAllBoards().forEach(
+                value -> fileWriterService.writeTeamsFile(value, uri)
+        );
+    }
+
+    public void WriteClassificationFile(String uri) {
+        val classification = orderByHighestPontuation(teamBoardRepository.getAllBoards());
+        classification.forEach(
+                board -> fileWriterService.writeBoard(board, uri));
+    }
+
+    private void fillsTeamTable() {
         val ordered = orderByDateAndName(matchRepository.getAllRegisters());
         val tableByClient = groupByClient(ordered);
         for (String key : tableByClient.keySet()) {
@@ -39,19 +48,8 @@ public class DataProcessing {
                 board.add(soccerMatch);
             }
             this.teamBoardRepository.add(board);
+            val teamBoards = this.teamBoardRepository.getAllBoards();
         }
-    }
-
-    public void WriteEachTeamsFile() {
-        teamBoardRepository.getAllBoards().forEach(
-                value -> fileWriterService.writeTeamsFile(value, URI_PATH_SAVE_TEAMS_FILE)
-        );
-    }
-
-    public void WriteClassificationFile() {
-        val classification = orderByHighestPontuation(teamBoardRepository.getAllBoards());
-        classification.forEach(
-                board -> fileWriterService.writeBoard(board, URI_PATH_SAVE_CLASSIFICATION_FILE));
     }
 
     private TreeMap<String, List<SoccerMatch>> groupByClient(TreeSet<SoccerMatch> ordered) {
@@ -69,5 +67,7 @@ public class DataProcessing {
         list.addAll(toOrder);
         return list;
     }
+
+
 
 }
